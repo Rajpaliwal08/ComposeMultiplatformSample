08cea37faf226097e63e7166a16be8fc8da69235
rajpaliwal08 <rajpaliwal0108@gmail.com>
2024-01-29 22:45:57 +0530

    complete the basic app

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "17"
             }
         }
     }
@@ -84,8 +84,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,62 +1,113 @@
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MovableContent
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.util.reflect.instanceOf
 import model.BirdImage
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+
+
+@Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+}
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        val greeting = remember { Greeting().greet() }
-        LaunchedEffect(Unit) {
-            println(getImages())
-        }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!, and see the magic")
-            }
-            AnimatedVisibility(showContent) {
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    KamelImage(
-                        asyncPainterResource(
-                            "https://sebi.io/demo-image-api/pigeon/nandkumar-patel-RPUD-n9V6E0-unsplash.jpg")
-                            ,"PIGEON"
-                    )
-                }
-            }
-        }
+    BirdAppTheme {
+        val birdsViewModel = getViewModel(Unit, viewModelFactory {
+            BirdsViewModel()
+        })
+        BirdsPage(birdsViewModel)
     }
 }
 
-val httpClient = HttpClient {
-    install(ContentNegotiation) {
-        json()
+@Composable
+fun BirdsPage(viewModel: BirdsViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+
+        Row(
+            Modifier.fillMaxWidth().padding(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            for (category in uiState.categories) {
+                Button(onClick = {
+                    viewModel.selectCategory(category)
+                }, modifier = Modifier.aspectRatio(1.0f).fillMaxSize().weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        focusedElevation = 0.dp
+                    )) {
+                    Text(category)
+                }
+            }
+        }
+
+    AnimatedVisibility(uiState.selectedImages.isNotEmpty()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.fillMaxSize().padding(5.dp),
+            content = {
+                items(uiState.selectedImages) {
+                    BirdImageCell(it)
+                }
+            }
+        )
     }
 }
-suspend fun getImages() : List<BirdImage> {
-    val images = httpClient
-        .get("https://sebi.io/demo-image-api/pictures.json")
-        .body<List<BirdImage>>()
-    return images
+}
+
+@Composable
+fun BirdImageCell(image: BirdImage) {
+    KamelImage(
+        asyncPainterResource("https://sebi.io/demo-image-api/${image.path}"),
+    "${image.category} by ${image.author}",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxWidth().aspectRatio(1.0f)
+    )
 }
